28415a5083f6a2ec6edcaf39278a6fa68b18680d
bluedabdidabdae <bluedabdidabdae@gmail.com>
2024-10-07 19:35:56 +0200

    did smth, not going well

--- src/game_engine.cpp
+++ src/game_engine.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
+#include <pthread.h>
 #include <ctime> // FOR LINUX
 // #include <time.h> // FOR WINDOWS
 
@@ -16,6 +17,10 @@
 int GameEngine()
 {
     srand(time(NULL));
+
+    pthread_t drawingThread;
+
+    DrawArgsS drawArgs;
 
     uint frameCounter = 0;
     Rectangle player = { WIDTH/2-20, HEIGT/2-20, 40, 40 };
@@ -46,17 +51,22 @@ int GameEngine()
                               {0, HEIGT, WIDTH+WALLTHICKNESS, WALLTHICKNESS},
                               {WIDTH, 0, WALLTHICKNESS, HEIGT+WALLTHICKNESS} };
 
+    drawArgs.camera = &camera;
+    drawArgs.currentEnemy = enemiesHead;
+    drawArgs.player = &player;
+    drawArgs.mapBorder = mapBorder;
+    drawArgs.projectileHead = projectileHead;
+
     while(1)
     {
-        // Updating camera target to the player position
-        camera.target = (Vector2){ player.x + 20, player.y + 20 };
-
         frameCounter++;
-        DrawGame(&camera, enemiesHead, &player, mapBorder, projectileHead);
+        pthread_create(&drawingThread, NULL, DrawGame, &drawArgs); 
+        //DrawGame(&camera, enemiesHead, &player, mapBorder, projectileHead);
 
         if(IsKeyPressed(KEY_M))
         {
             //if(GameMenuHandler() == 3)
+            pthread_join(drawingThread, NULL);
             CompletelyDeleteAllEnemies(enemiesHead);
             CompletelyDeleteAllProjectiles(projectileHead);
             return 0;
@@ -72,42 +82,49 @@ int GameEngine()
             UpdateProjectiles(projectileHead);
             CheckProjectilesBorders(projectileHead, mapBorder);
         }
+        pthread_join(drawingThread, NULL);
     }
 }
 
-void DrawGame(Camera2D *camera, EnemyLL *currentEnemy, Rectangle *player, Rectangle mapBorder[], ProjectileLL *projectileHead)
+void* DrawGame(void* dataPointer)
 {
+    DrawArgsS *data = (DrawArgsS*)dataPointer;
+
+    // Updating camera target to the player position
+    data->camera->target = (Vector2){ data->player->x + 20, data->player->y + 20 };
+
     BeginDrawing();
         ClearBackground(BLACK);
-
-        BeginMode2D(*camera);
+        BeginMode2D(*data->camera);
             
             // drawing projectiles
-            while(projectileHead->next != NULL)
+            while(data->currentEnemy->next != NULL)
             {
-                DrawRectangleRec(projectileHead->projectile, projectileHead->color);
-                projectileHead = projectileHead->next;
+                DrawRectangleRec(data->projectileHead->projectile, data->projectileHead->color);
+                data->projectileHead = data->projectileHead->next;
             }
 
             // drawing map borders
             for(int i = 0; i < 4; i++)
-                DrawRectangleRec(mapBorder[i], BLUE);
+                DrawRectangleRec(data->mapBorder[i], BLUE);
 
             // drawing enemies from linked list of type *EnemyLL
-            while(currentEnemy->next != NULL)
+            while(data->currentEnemy->next != NULL)
             {
-                currentEnemy = currentEnemy->next;
-                DrawRectangleRec(currentEnemy->healthBar, RED);
-                DrawRectangle(currentEnemy->healthBar.x,
-                              currentEnemy->healthBar.y,
-                              currentEnemy->hitPoint,
-                              currentEnemy->healthBar.height,
+                data->currentEnemy = data->currentEnemy->next;
+                DrawRectangleRec(data->currentEnemy->healthBar, RED);
+                DrawRectangle(data->currentEnemy->healthBar.x,
+                              data->currentEnemy->healthBar.y,
+                              data->currentEnemy->hitPoint,
+                              data->currentEnemy->healthBar.height,
                               GREEN);
-                DrawRectangleRec(currentEnemy->enemy, currentEnemy->color);
+                DrawRectangleRec(data->currentEnemy->enemy, data->currentEnemy->color);
             }
             
             // drawing player
-            DrawRectangleRec(*player, RAYWHITE);
+            DrawRectangleRec(*data->player, RAYWHITE);
         EndMode2D();
     EndDrawing();
+
+    pthread_exit(NULL);
 }
